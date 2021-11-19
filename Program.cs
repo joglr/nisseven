8d1927f48251a99717e70b549f43de69ba02c91d
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-11-19 21:42:42 +0100

    Add user help

--- Program.cs
+++ Program.cs
@@ -14,9 +14,11 @@ var WriteSnow = (String write) => {
   Console.WriteLine($"⛄ {write}");
 };
 
+var doneString = "miskidut";
+
 Console.WriteLine("Welcome christmas-nisse!🎅🏼");
 Console.WriteLine("");
-Console.WriteLine("Write miskidut to exit!!");
+Console.WriteLine($"Write {doneString} to exit!!");
 Console.WriteLine("Add new name:");
 var done = false;
 List<string> persons = new List<string>();
@@ -26,10 +28,10 @@ while (done == false)
   var input = Console.ReadLine();
   if (input == null || input == "")
   {
-    Console.WriteLine("Please input a name");
+    Console.WriteLine($"Please input a name or write {doneString} when done");
     continue;
   }
-  if (input.ToLower() == "miskidut")
+  if (input.ToLower() == doneString)
   {
     done = true;
     continue;
