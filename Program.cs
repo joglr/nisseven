e2e440df934bcfb788067a5ca98ee2ea5248304d
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-11-19 21:18:50 +0100

    Added persons

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+CMLTool
+Opsætningsface:
+  Indskrivning af personer
+  Pulje af personer
+  Personerne
+*/
+using System.Collections.Generic;
+Console.Clear();
+Console.WriteLine("Welcome christmas-nisse!");
+Console.WriteLine("Write miskidut to exit!!");
+Console.WriteLine("Add new name:");
+var done = false;
+List<string> persons = new List<string>();
+
+while(done == false){
+  var input = Console.ReadLine();
+  if(input == "miskidut"){done = true; continue;}
+  if(input == null){
+    Console.WriteLine("Please input a name");
+    continue;
+  }
+  persons.Add(input);
+}
+foreach(var person in persons){
+  Console.WriteLine(person);
+}
